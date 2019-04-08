81325747623288ba919595924fc5e03f2cc2f849
za18acr <za18acr@ENT-D410-24.herts.ac.uk>
2019-04-08 13:39:20 +0100

    First Test Case applied and passed "setTitle to empty string

--- test/embeddedmediaplayer/ClipTest.java
+++ test/embeddedmediaplayer/ClipTest.java
@@ -43,6 +43,26 @@ public class ClipTest {
     @Test
     public void testSetTitleToEmptyStringKeepsPreviousValue()
     {
+        Clip instanceClip1 = new Clip(); // creating new clip for the class
+        Clip instanceClip2 = new Clip(); // creating another new clip for the class
+        System.out.println("Set title to empty string (Keeps previous value)");
+        boolean emptyStringFlag = false, nullStringFlag = false;
+        
+        // Take the title before modified and try to change the name with empty String.
+        String initialTitle = instanceClip1.getTitle(); //Get the real name of the sub-clip before made any changes.
+        instanceClip1.setTitle("");  // set the title as empty string
+        String initialTitleAftermodified = instanceClip1.getTitle(); //Get the real name of the sub-clip after made changes.
+        if (initialTitle.equals(initialTitleAftermodified))
+            emptyStringFlag = true;
+        
+        // Take the title before modified and try to change the name with null String.
+        initialTitle = instanceClip1.getTitle(); //Get the real name of the sub-clip before made any changes.
+        instanceClip1.setTitle(null); //// set the title as null
+        initialTitleAftermodified = instanceClip1.getTitle(); //Get the real name of the sub-clip after made changes.
+        if (initialTitle.equals(initialTitleAftermodified))
+            nullStringFlag = true;
+        
+        assertEquals(true,emptyStringFlag&&nullStringFlag);
     }
 
     @Test
